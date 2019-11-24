476cca616b2ff2487668e00d4364368c6c50d284
Aj Wietecha II <ajwietechaii@yahoo.com>
2019-11-24 14:28:31 -0800

    added in new components

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Post } from '../post.model';
 
 @Component({
     selector: 'app-post-list',
@@ -14,5 +15,5 @@ export class PostListComponent {
     //     {title: 'Fifth Post', content: 'This is the fifth post\'s contnent!'},
     //     {title: 'Sixth Post', content: 'This is the sixth post\'s contnent!'},
     // ];
-    posts = [];
+    @Input() posts: Post[] = [];
 }
