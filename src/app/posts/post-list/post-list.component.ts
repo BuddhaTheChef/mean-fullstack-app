1e737a9cf0aab6ba9bbfa74574b9035411141bb3
Aj Wietecha II <ajwietechaii@yahoo.com>
2019-11-23 17:05:52 -0800

    added in more components

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-post-list',
+    templateUrl: './post-list.component.html',
+    styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent {
+    // posts = [
+    //     {title: 'First Post', content: 'This is the first post\'s contnent!'},
+    //     {title: 'Second Post', content: 'This is the second post\'s contnent!'},
+    //     {title: 'Third Post', content: 'This is the third post\'s contnent!'},
+    //     {title: 'Fourth Post', content: 'This is the fourth post\'s contnent!'},
+    //     {title: 'Fifth Post', content: 'This is the fifth post\'s contnent!'},
+    //     {title: 'Sixth Post', content: 'This is the sixth post\'s contnent!'},
+    // ];
+    posts = [];
+}
